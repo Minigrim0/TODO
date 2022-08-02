bd6e7d40de7e61aeef124c00b2e7c197ad3f7187
Minigrim0 <grimauflorent@gmail.com>
2022-08-02 09:00:04 +0200

    [Add] Commands header + initial help

---
+++ src/print_help.cpp
@@ -0,0 +1,9 @@
+#include "commands.hpp"
+
+#include <iostream>
+
+void print_help(){
+    std::cout << "TODO:" << std::endl;
+    std::cout << "  -h,    --help: Prints this help message" << std::endl;
+    std::cout << "  -v, --version: Prints the version of the program" << std::endl;
+}
