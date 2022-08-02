2a46f6542655a348b02dd7753648bef5a2dc2a55
Minigrim0 <grimauflorent@gmail.com>
2022-08-02 08:59:49 +0200

    [Upd] test initial parser

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <sqlite3.h>
+
+#include "src/parser.hpp"
+
 
 int main(int argc, char *argv[])
 {
     std::cout << "Hello, World!" << std::endl;
+    std::map<std::string, std::string> parsed;
+    parse_arguments(argc, argv, parsed);
     return 0;
 }
