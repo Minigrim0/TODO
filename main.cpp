4c9454149e988545c266f28d7ee78aa25a51339f
Minigrim0 <grimauflorent@gmail.com>
2022-11-22 15:55:18 +0100

    [Upd] Main + added vscode config for debugging

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <sqlite3.h>
+#include <list>
 
 #include "src/parser.hpp"
 
 
-int main(int argc, char *argv[])
-{
-    std::cout << "Hello, World!" << std::endl;
-    std::map<std::string, std::string> parsed;
-    parse_arguments(argc, argv, parsed);
+int main(int argc, char *argv[]){
+    std::list<std::string> parsed;
+    parse_arguments(argc, argv, &parsed);
+    std::cout << "Parsed arguments: " << std::endl;
+    for(std::string arg : parsed){
+        std::cout << arg << std::endl;
+    }
     return 0;
 }
