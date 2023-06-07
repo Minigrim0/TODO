7a3837247779d3e716d70fe3eb0d90d2a2e11bdd
Minigrim0 <grimauflorent@gmail.com>
2023-06-07 16:17:31 +0200

    [Upd] Added diesel orm dependency

---
+++ migrations/2023-06-07-134015_create_tasks/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    status boolean DEFAULT false NOT NULL -- false = not done, true = done
+);
