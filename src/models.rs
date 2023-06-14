fb98b2bd9e11eeadc572bf9e63773444952fe06d
Minigrim0 <grimauflorent@gmail.com>
2023-06-14 16:56:14 +0200

    [Fix] Made task cloneable

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,6 @@
 use diesel::prelude::*;
 
-#[derive(Queryable, Selectable)]
+#[derive(Queryable, Selectable, Clone)]
 #[diesel(table_name = crate::schema::tasks)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Task {
