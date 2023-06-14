0baa222772535a9c49494783a0753cb8d68a7b65
Minigrim0 <grimauflorent@gmail.com>
2023-06-14 16:56:30 +0200

    [Upd] Hand over display to the parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use colored::Colorize;
+
 use crate::cli::Cli;
 use crate::tasks;
 
@@ -10,10 +12,30 @@ pub fn parse_args(args: Cli) {
             tasks::cmd_add_task(task_name, task_description);
         }
         Cli {task_to_delete: Some(task_id), ..} => {
-            tasks::cmd_delete_task(task_id)
+            if tasks::cmd_delete_task(task_id) {
+                println!(
+                    "{} {}",
+                    "Successfully deleted task".green(),
+                    task_id.to_string().green().bold()
+                )
+            } else {
+                println!(
+                    "{} {}",
+                    "An error occured while deleting task".yellow(),
+                    task_id.to_string().yellow().bold()
+                )
+            }
         }
         Cli {task_to_complete: Some(task_id), ..} => {
-            tasks::cmd_complete_task(task_id)
+            if tasks::cmd_complete_task(task_id) {
+                println!(
+                    "{} {} {}",
+                    "Successfully marked task".green().bold(),
+                    task_id.to_string().green().bold(),
+                    "as completed".green().bold()
+                )
+            }
+            tasks::cmd_display_tasks(false);
         }
         _ => {  // Print usage
             tasks::cmd_unknown();
