6e5f1d881d942a3c2613154abada928267d0e0b8
Minigrim0 <grimauflorent@gmail.com>
2023-06-14 16:55:57 +0200

    [Add] some tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,18 +1,53 @@
 #[cfg(test)]
 mod tests {
     use crate::tasks;
-    use crate::database;
 
     #[test]
     fn create_task() {
         let task_name: String = "Testing Task 1".to_string();
-        let description: String = "Description of the test task".to_string();
+        let description: String = "This task is created".to_string();
 
-        tasks::cmd_add_task(
-            task_name,
+        let inserted_task = tasks::cmd_add_task(
+            task_name.clone(),
             Some(description)
         );
 
-        assert_eq!(database::read_tasks(false).len(), 1);
+        assert_eq!(inserted_task.title, task_name);
+    }
+
+    #[test]
+    fn close_task() {
+        let task_name: String = "Testing task 2".to_string();
+        let task_description: String = "This task should be completed".to_string();
+
+        let inserted_task = tasks::cmd_add_task(
+            task_name.clone(),
+            Some(task_description).clone()
+        );
+
+        assert_eq!(tasks::cmd_complete_task(inserted_task.id), true);
+    }
+
+    #[test]
+    fn delete_task() {
+        let task_name: String = "Testing task 2".to_string();
+        let task_description: String = "This task should be deleted".to_string();
+
+        let inserted_task = tasks::cmd_add_task(
+            task_name.clone(),
+            Some(task_description).clone()
+        );
+
+        assert_eq!(tasks::cmd_delete_task(inserted_task.id.clone()), true);
+    }
+
+    #[test]
+    fn update_task_name() {
+
+    }
+
+    #[test]
+    fn update_task_description() {
+
     }
 }
