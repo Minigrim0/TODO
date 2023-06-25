ed38704348a9bc4a3c7e76d8ad30c688a5a50b23
Minigrim0 <grimauflorent@gmail.com>
2023-06-25 13:55:32 +0200

    [Fix] tests

--- src/tests.rs
+++ src/tests.rs
@@ -6,10 +6,12 @@ mod tests {
     fn create_task() {
         let task_name: String = "Testing Task 1".to_string();
         let description: String = "This task is created".to_string();
+        let date: String = "01-01-1970".to_string();
 
         let inserted_task = tasks::cmd_add_task(
             task_name.clone(),
-            Some(description)
+            Some(description),
+            Some(date)
         );
 
         assert_eq!(inserted_task.title, task_name);
@@ -19,10 +21,12 @@ mod tests {
     fn close_task() {
         let task_name: String = "Testing task 2".to_string();
         let task_description: String = "This task should be completed".to_string();
+        let date: String = "01-01-1970".to_string();
 
         let inserted_task = tasks::cmd_add_task(
             task_name.clone(),
-            Some(task_description).clone()
+            Some(task_description).clone(),
+            Some(date)
         );
 
         assert_eq!(tasks::cmd_complete_task(inserted_task.id), true);
@@ -32,10 +36,12 @@ mod tests {
     fn delete_task() {
         let task_name: String = "Testing task 2".to_string();
         let task_description: String = "This task should be deleted".to_string();
+        let date: String = "01-01-1970".to_string();
 
         let inserted_task = tasks::cmd_add_task(
             task_name.clone(),
-            Some(task_description).clone()
+            Some(task_description).clone(),
+            Some(date)
         );
 
         assert_eq!(tasks::cmd_delete_task(inserted_task.id.clone()), true);
@@ -45,10 +51,12 @@ mod tests {
     fn update_task_name() {
         let task_title: String = "Testing task 2.5".to_string();
         let task_description: String = "This is the testing task 3!".to_string();
-        
+        let date: String = "01-01-1970".to_string();
+
         let inserted_task = tasks::cmd_add_task(
             task_title.clone(),
-            Some(task_description.clone())
+            Some(task_description.clone()),
+            Some(date)
         );
 
         let new_title: String = "Testing task 3".to_string();
@@ -66,10 +74,12 @@ mod tests {
     fn update_task_description() {
         let task_title: String = "Testing task 3".to_string();
         let task_description: String = "This is the testing task 2!".to_string();
+        let date: String = "01-01-1970".to_string();
 
         let inserted_task = tasks::cmd_add_task(
             task_title.clone(),
-            Some(task_description)
+            Some(task_description),
+            Some(date)
         );
 
         let new_description: String = "This is the testing task 3!".to_string();
